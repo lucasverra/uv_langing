9511372f2553ea0c6173941e8f40392e169be5d7
codebreaker <sign4designs@gmail.com>
2018-03-02 00:20:55 +0530

    update the animation

--- assets/js/main.js
+++ assets/js/main.js
@@ -31,6 +31,30 @@
 				languageSelector();
 				textTypeAnimation.init('#test-data', 1000);
 				textTypeAnimation.init('#test-info', 1000);
+
+
+				$(window).scroll( function(){
+    
+			        /* Check the location of each desired element */
+			        $('.feature').each( function(i){
+			            console.log($(this));
+			            var bottom_of_object = $(this).position().top + $(this).outerHeight();
+			            var bottom_of_window = $(window).scrollTop() + $(window).height();
+			            
+			            /* If the object is completely visible in the window, fade it it */
+			            if( bottom_of_window > bottom_of_object ){
+			                
+			                $(this).addClass('active');
+			                    
+			            }
+			            
+			        }); 
+			    
+			    });
+
+
+
+
 			});
 
 		// Fix: Placeholder polyfill.
